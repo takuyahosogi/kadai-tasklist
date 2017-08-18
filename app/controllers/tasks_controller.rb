164ccfc5456b25e6c8baa2d1a641521ed61ce6ba
takuya hosogi <t.hosogi26@gmail.com>
2017-08-18 03:30:22 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,7 +48,7 @@ class TasksController < ApplicationController
   end
   
    def task_params
-    params.require(:task).permit(:status)
+    params.require(:task).permit(:content ,:status)
   end
   private
 
