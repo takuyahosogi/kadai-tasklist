7fb2a24c16bcb6ba34ba9736c64c5d21fbafd01d
takuya hosogi <t.hosogi26@gmail.com>
2017-09-05 07:18:25 +0000

    post

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
+  belongs_to :user
   validates :content, presence: true, length: { maximum: 10 }
   validates :status, presence: true, length: { maximum: 10 }
-  belongs_to :user
+
   validates :user_id, presence: true
 end
