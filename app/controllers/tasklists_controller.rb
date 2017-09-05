eb31d76a77425bca75c3ef10c0e64045d0d9372f
takuya hosogi <t.hosogi26@gmail.com>
2017-09-05 13:57:35 +0000

    user

--- app/controllers/tasklists_controller.rb
+++ app/controllers/tasklists_controller.rb
@@ -1,6 +1,6 @@
 class TasklistsController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:show, :edit, :destroy]
+  before_action :correct_user, only: [:show, :update, :destroy]
   
   def create
     @task = current_user.tasks.build(task_params)
