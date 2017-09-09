37f2459a8e5a4d3b2a3290528b24205c87667248
takuya hosogi <t.hosogi26@gmail.com>
2017-09-09 04:58:18 +0000

    current user

--- app/controllers/tasklists_controller.rb
+++ app/controllers/tasklists_controller.rb
@@ -1,6 +1,6 @@
 class TasklistsController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:show, :update, :destroy]
+  before_action :current_user, only: [:show, :update, :destroy]
   
   private
 
