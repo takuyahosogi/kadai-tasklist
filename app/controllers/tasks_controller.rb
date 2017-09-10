f04a62bce95f15cfeccb9d8407aeeb934153ce95
takuya hosogi <t.hosogi26@gmail.com>
2017-09-10 16:01:39 +0000

    logout

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -54,5 +54,4 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content ,:status)
   end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'tasklists/create'
 
   get 'tasklists/destroy'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'toppages#index'
